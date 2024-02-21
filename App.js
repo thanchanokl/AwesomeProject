bf825b38781d6fc292d1c448b7b7c24fb5e51b13
thanchanokl <94107371+thanchanokl@users.noreply.github.com>
2024-02-21 16:16:53 +0700

    Revert "week3+4"
    This reverts commit d5cde6379ca4efd465c587b20918e0a227662730.

--- App.js
+++ App.js
@@ -16,18 +16,7 @@ import Ex11 from './screens/week2/Ex11';
 import Ex12 from './screens/week2/Ex12';
 import Extra from './screens/week2/Extra';
 import Extra2 from './screens/week2/Extra2';
-import { Alert, Button, Image, ScrollView, ScrollViewBase, Text, TextInput, View } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
-import Greeting from './components/Greeting';
-import Travel from './screens/week3/Travel';
-import Signup from './components/week3/Signup';
-import Card from './components/week3/Card';
-import Hotel from './components/week3/Hotel';
-import MyIcon from './components/week3/MyIcon';
-import Resort from './screens/week3/Resort';
-import { PaperProvider } from 'react-native-paper';
-import TestPaper from './screens/week4/TestPaper';
-import PaperQuiz from './screens/week4/PaperQuiz';
+
 
 export default function App() {
   return (
@@ -45,80 +34,7 @@ export default function App() {
     // <Ex11/>
     // <Ex12/>
     // <Extra/>
-    // <Extra2/>
-    // <Travel/>
-    // <Resort/>
-    // <PaperProvider>
-    //   <TestPaper/>
-    // </PaperProvider>
-   <PaperProvider>
-     <PaperQuiz/>
-   </PaperProvider>
-    
-    // <ScrollView>
-    // <View style = {{ flex:1, marginTop:25 }}>
-    // {/* <View style={{ flex: 1, flexDirection: 'column' }}> */}
-    
-    //   <TextInput
-    //     keyboardType="email-address"
-    //     autoCapitalize="none"
-    //     autoCorrect={false}
-    //     maxLength={30}
-    //     multiline={false}
-    //     placeholder="Please enter your email"
-    //     returnKeyType="next"
-    //     onChangeText={(text)=>{}}
-    //     defaultValue="Default text"
-    //   />
-      
-    //   <Button
-    //     title="Press me"
-    //     onPress={() => Alert.alert("Simple Button pressed")}
-    //     color="#665544"
-    //   />
-      
-    //   <Text> Hello World </Text>
-      
-    //   <Image source={{ uri : 'https://reactnative.dev/img/tiny_logo.png' }} style={{ width:50, height:50 }} />
-    //   <Image source={{ uri : 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==' }} style={{ width:50, height:50 }} />
-      
-    //   <View style={{ flex: 1, flexDirection: 'column' }}>
-    //     {/* Fixed Size */}
-    //     <View style={{ flexDirection: 'row' , justifyContent : 'space-between' , marginTop : 50}}>
-    //       <Image style={{ width: 150, height: 100 }} source={{ uri : 'https://reactnative.dev/img/tiny_logo.png' }} />
-    //       <Image style={{ width: 150, height: 150}} source={{ uri : 'https://reactnative.dev/img/tiny_logo.png' }} />
-    //       <Image style={{ width: 150, height: 150, borderRadius: 150/2 }} source={{ uri : 'https://reactnative.dev/img/tiny_logo.png' }} />
-    //     </View>    
-    //   </View>
-
-    //   <View style={{ flex: 1, flexDirection: 'column' }}>
-    //     <View style={{ flexDirection: 'row' , marginTop : 20 }}>
-    //       <Image style={{ flex: 1, resizeMode: 'cover', aspectRatio: 3 / 2 }} source={{ uri : 'https://reactnative.dev/img/tiny_logo.png' }} />
-    //     </View>
-    //     <View style={{ flexDirection: 'row' , marginTop : 20 }}>
-    //       <Image style={{ flex: 1, resizeMode: 'cover', aspectRatio: 16 / 4 }} source={{ uri : 'https://reactnative.dev/img/tiny_logo.png' }} />
-    //     </View>
-    //     <View style={{ flexDirection: 'row' , marginTop : 20, backgroundColor : 'gray' }}>
-    //       <Image style={{ flex: 1, resizeMode: 'contain', aspectRatio: 16 / 4 }} source={{ uri : 'https://reactnative.dev/img/tiny_logo.png' }} />
-    //     </View>
-    //   </View>
-      
-    //   <FontAwesome name="heart"  size={32} color="orange" />
-
-    //   {/* <View style={{ height: 50, width: 50, borderRadius: 50/2, backgroundColor: 'red', justifyContent: 'center', alignItems: 'center' }}>
-    //     <Text style={{ fontSize: 30, color : "white" }}>หัวใจ</Text>
-    //   </View> */}
-
-    //   {/* <View style={{alignItems: 'center', top: 50}}> */}
-    //     <Greeting name='Rexxar' />
-    //     <Greeting name='Jaina' />
-    //     <Greeting name='Valeera' />
-    //   {/* </View> */}
-
-    // {/* </View> */}
-    // </View>
-    // </ScrollView>
-
-    );
+    <Extra2/>
+  );
 }
 
--- components/Greeting.js
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-export default function Greeting(props){
-    return (
-      <View style={{alignItems: 'center'}}>
-        <Text>Hello {props.name} !</Text>
-        {/* <Text>Hello World</Text> */}
-      </View>
-    );
-}
